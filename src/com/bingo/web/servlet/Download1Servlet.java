587662cb915089314ea7ec9c54df05299a8f3060
bingo906 <906014227@qq.com>
2017-04-26 14:20:47 +0800

    多种浏览器下载名编码判断

--- src/com/bingo/web/servlet/Download1Servlet.java
+++ src/com/bingo/web/servlet/Download1Servlet.java
@@ -3,6 +3,7 @@ package com.bingo.web.servlet;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -11,6 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
+
+import sun.misc.BASE64Encoder;
 
 public class Download1Servlet extends HttpServlet {
 
@@ -24,7 +27,7 @@ public class Download1Servlet extends HttpServlet {
 			 */
 		String filename = "F:/流程.jpg";
 		//解决下载名中文编码问题
-		String framenname = new String("流程.jpg".getBytes("GBK"),"ISO-8859-1");
+		String framenname = filenameEncoding("流程.jpg",request);
 		FileInputStream input = new FileInputStream(filename);
 		String contentType = this.getServletContext().getMimeType(filename);
 		String contentDisposition = "attachment;filename="+framenname;
@@ -41,4 +44,19 @@ public class Download1Servlet extends HttpServlet {
 		doGet(request, response);
 	}
 
+	// 用来对下载的文件名称进行编码的！
+	public static String filenameEncoding(String filename, HttpServletRequest request) throws IOException {
+		String agent = request.getHeader("User-Agent"); //获取浏览器
+		if (agent.contains("Firefox")) {
+			BASE64Encoder base64Encoder = new BASE64Encoder();
+			filename = "=?utf-8?B?"
+					+ base64Encoder.encode(filename.getBytes("utf-8"))
+					+ "?=";
+		} else if(agent.contains("MSIE")) {
+			filename = URLEncoder.encode(filename, "utf-8");
+		} else {
+			filename = URLEncoder.encode(filename, "utf-8");
+		}
+		return filename;
+	}
 }
